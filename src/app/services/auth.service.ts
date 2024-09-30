bdcc62ff6cb18ed58f59ac79fd3b1f5bd410f6c3
AgilSpotPay <134330922+AgilSpotPay@users.noreply.github.com>
2024-09-30 18:31:29 -0300

    OUTRO

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -7,23 +7,23 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'https://hub.admspot.com/cafeapi/me'; // URL da API
+  private apiUrl = 'https://www.hub.admspot.com/cafeapi/me'; // URL da API
 
   constructor(private http: HttpClient) { }
 
   // Método para autenticar o usuário
   authenticate(email: string, password: string): Observable<any> {
     const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'email': email,
-      'password': password,
+      'email': 'cesar@agilspotpay.com.br',
+      'password': 'ab31hgTT!',
+      // 'Cookie': 'PHPSESSID=4aaef4124768e117535824342e6593fd'
     });
 
     // Enviando a requisição GET com os cabeçalhos
-    // return this.http.get<any>(this.apiUrl, { headers });
     return this.http.get(this.apiUrl, {
       headers: headers,
-      withCredentials: true  // Certifique-se de que as credenciais estão sendo enviadas
+      withCredentials: true, // Enviar cookies e credenciais
+      observe: 'body'        // Observa apenas o corpo da resposta
     })
       .pipe(
         catchError((error) => {
